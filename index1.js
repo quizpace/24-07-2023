b942c49fd2bca78ed8ece371c1069152abcacd0a
Avi <drazninavi@gmail.com>
2023-07-24 20:20:47 +0300

    update

---
+++ index1.js
@@ -0,0 +1,36 @@
+
+var sum, diff, mul, the_div, power2a, power2b, avg, sqrt_a, sqrt_b
+var a = 4
+var b = 5
+
+sum = a + b; // 4 + 5 = 9
+diff = a - b;
+mul = a * b;
+the_div = a / b;
+power2a = a * a;
+power2b = b * b;
+avg = (a + b) / 2; 
+sqrt_a = Math.sqrt ( a )
+sqrt_b = Math.sqrt ( b )
+
+// power2 of a 
+// power2 of b
+// avg of a and b
+// *bonus: sqrt a
+// *bonus: sqrt b
+
+document.write("<h3>Hello JS! </h3>");
+document.write("a + b = ", sum);
+document.write("<br /> a - b = ", diff);
+document.write("<br /> a * b = ", mul);
+document.write("<br /> a / b = ", the_div);
+// add the results here
+
+document.write("<br /> a * a = ", power2a);
+document.write("<br /> b * b = ", power2b);
+document.write("<br /> (a + b) / 2 = ", avg);
+document.write("<br /> a sqrt = ", sqrt_a);
+document.write("<br /> b sqrt = ", sqrt_b);
+
+
+
---
+++ index2.js
@@ -0,0 +1,2 @@
+
+Document.write("8 / ")
